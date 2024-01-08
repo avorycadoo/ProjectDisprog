c1b9c08facd20382773fb85d03a16b79d00aac3c
Styaaa <s160421048@student.ubaya.ac.id>
2024-01-08 19:58:53 +0800

    ingpo

--- WebServiceProjectUASDisprog/src/java/newpackage/model/WebServiceServer.java
+++ WebServiceProjectUASDisprog/src/java/newpackage/model/WebServiceServer.java
@@ -4,6 +4,7 @@
  */
 package newpackage.model;
 
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -15,11 +16,23 @@ import javax.jws.WebParam;
 @WebService(serviceName = "WebServiceServer")
 public class WebServiceServer {
 
+    user user;
+    ArrayList<Object> listofuser;
     /**
      * This is a sample web service operation
      */
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "login")
+    public Boolean login(@WebParam(name = "username") String username, @WebParam(name = "password") String password) {
+        //TODO write your implementation code here:
+        
+        return user.checkLogin(username, password);
     }
 }
